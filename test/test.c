62070765b724731383ffe976ea6a533aedf55715
Hans Fugal <hans@fugal.net>
2008-07-09 19:08:39 -0600

    const stuff

--- test/test.c
+++ test/test.c
@@ -1,9 +1,9 @@
 #include "opts.h"
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int main(int argc, char *const *argv)
 {
-    struct options opts = {};
+    struct options opts = {0};
     printf("argc %d argv %p\n", argc, argv);
     parse_options(&argc, &argv, &opts);
     printf("f %d\nb %s\nz %d\nq %f\n", opts.f, opts.b, opts.z, opts.q);
