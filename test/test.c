94ca3d17d68d49ff27388adf52887bef5715bc5f
Hans Fugal <hans@fugal.net>
2008-07-09 18:37:05 -0600

    actually modify argv

--- test/test.c
+++ test/test.c
@@ -4,6 +4,8 @@
 int main(int argc, char **argv)
 {
     struct options opts = {};
-    parse_options(&argc, argv, &opts);
+    printf("argc %d argv %p\n", argc, argv);
+    parse_options(&argc, &argv, &opts);
     printf("f %d\nb %s\nz %d\nq %f\n", opts.f, opts.b, opts.z, opts.q);
+    printf("argc %d argv %p\n", argc, argv);
 }
